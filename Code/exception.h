9451794a686543c17b3b23ee86c045650c37a3ea
Gabrielle van de Vijver <gabrielle.vandevijver@gmail.com>
2022-11-07 16:55:12 +0100

    ajout classe Exception

---
+++ Code/exception.h
@@ -0,0 +1,15 @@
+#ifndef EXCEPTION_H_INCLUDED
+#define EXCEPTION_H_INCLUDED
+
+#include <string>
+#include <iostream>
+using namespace std;
+class SetException{
+private:
+string info;
+public:
+SetException(const string& s):info(s){};
+string getException(){return info;}
+};
+
+#endif // EXCEPTION_H_INCLUDED
