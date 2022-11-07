ab672ad0546d3a564824e05db0d8fc030a053568
Gabrielle van de Vijver <gabrielle.vandevijver@gmail.com>
2022-11-07 17:07:47 +0100

    main.cpp

--- Code/main.cpp
+++ Code/main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     const string& txt = "Pendant le tour de n'importe quel joueur recevez une piece de la banque.";
-    Etablissement c0=Etablissement("Ferme",txt,Couleur::bleu,1,2,Type::agriculture,1);
-    cout<<&c0<<endl;
+    Etablissement e0=Etablissement("Ferme",txt,Couleur::bleu,1,2,Type::agriculture,1);
+    cout<<&e0<<endl;
     return 0;
 };
