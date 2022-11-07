7ff67646191319f9f91f62669e5f41fb4ab6b31e
Gabrielle van de Vijver <gabrielle.vandevijver@gmail.com>
2022-11-07 18:43:35 +0100

    ajout de la classe Pile_Etablissement

---
+++ Code/pile_etablissement.h
@@ -0,0 +1,27 @@
+#include "etablissement.h"
+#include <iostream>
+
+class Pile_Etablissement{
+    private:
+    const Etablissement* etablissement;
+    unsigned int effectif;
+    public:
+    void retirerCarte(unsigned int nb_cartes=1){
+        if(nb_cartes>effectif) throw SetException("nombre de cartes a retirer trop eleve");
+        else effectif-=nb_cartes;
+    }
+    void ajouterCarte(unsigned int nb_cartes=1){
+        effectif+=nb_cartes;
+    }
+    const Etablissement* getEtablissement()const{return etablissement;}
+    unsigned int getEffectif()const {return effectif;}
+    ~Pile_Etablissement()=default;
+    Pile_Etablissement(Etablissement* et,unsigned int ef = 1):etablissement(et),effectif(ef){};
+
+};
+
+ostream& operator<<(ostream& f,Pile_Etablissement& pile){
+ f<<"Pile de "<<pile.getEffectif()<<" exemplaire(s) de l'etablissement suivant :"<<endl<<endl<<pile.getEtablissement()<<endl;
+ //<<endl<<pile.getEtablissement();
+return f;
+};
