8fd042b607c98960b73806d86f503694338ce758
agatheeber <92367538+agatheeber@users.noreply.github.com>
2022-11-07 10:31:29 +0100

    setter

--- Code/joueur.h
+++ Code/joueur.h
@@ -8,25 +8,34 @@
 
 class Joueur{
     int id;
-    char* pseudo;
+    string pseudo;
     int de=1;
     int de_lance;
     int compte=3;
-    int placement_StartUp;
+    int placement_StartUp=0;
     static const int max_joueurs = 4;
     static const int min_joueurs = 2;
     Monument* monuments[nb_monuments];
     friend Partie;
-    public:
+
+public:
     Joueur();
     ~Joueur();
+    
+    //Getter
     int getNbDes() const;
     int getCompte() const;
-    static int getNbMaxJoueurs();
-    static int getNbMaxJoueurs();
-    int setNbDes(int nbDe);
-    int setCompte(int montant);
     int getPlacement_StartUp() const;
+    static int getNbMaxJoueurs();
+    static int getNbMaxJoueurs();
+    
+    //Setter
+    void setPseudo(string pseudo);
+    void setPlacement_StartUp(int placement_StartUp);
+    void setNbDes(int nbDe);
+    void setCompte(int montant);
+    
+    //Autres méthodes
     void ajouterMontant(int montant);
     bool victoire();
     int lancerDés();
