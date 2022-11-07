0d830ccb607b103f0cb30161e8a87badd16704e3
Gabrielle van de Vijver <gabrielle.vandevijver@gmail.com>
2022-11-07 16:50:08 +0100

    ajout conversions to string

--- Code/TypesEnum.h
+++ Code/TypesEnum.h
@@ -1,23 +1,35 @@
 #ifndef TYPESENUM_H
 #define TYPESENUM_H
-
-enum class Type {
-    elevage, 
-    agriculture, 
-    commerce
-};
+#include<string>
+#include "exception.h"
+using namespace std;
 
 enum class Extension {
-    Marina, 
-    GreenValley, 
+    Marina,
+    GreenValley,
     Deluxe
 };
 
-enum class Couleur {
-    Bleu,
-    Vert,
-    Rouge,
-    Violet
-};
+enum Type{agriculture,magasin,restauration,rouage};
+string toString(Type t) {
+		switch (t) {
+		case Type::agriculture: return "agriculture";
+		case Type::magasin: return "magasin";
+		case Type::restauration: return "restauration";
+		case Type::rouage: return "rouage";
+		default: throw SetException("Type inconnu");
+		}
+	}
+enum Couleur{bleu,rouge,violet,vert};
+string toString(Couleur c) {
+		switch (c) {
+		case Couleur::bleu: return "bleu";
+		case Couleur::rouge: return "rouge";
+		case Couleur::violet: return "violet";
+		case Couleur::vert: return "vert";
+		default: throw SetException("Couleur inconnue");
+		}
+	}
+
 
 #endif
