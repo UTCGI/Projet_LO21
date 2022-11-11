4a4032f98d847dfd0c7a77868ad668c53e1cdcbd
TobiasInfo <savarytobias@hotmail.com>
2022-11-11 15:55:40 +0100

    Rewiew carte_monument + pile_etablissement + creation pile_monument

--- Code/carte_monument.cpp
+++ Code/carte_monument.cpp
@@ -3,3 +3,9 @@
 
 CarteMonument::CarteMonument(Monument* monument)
     : monument(monument), construit(0) {}
+
+// Getters
+bool CarteMonument::estConstruit() const { return construit; }
+
+// Methods
+void CarteMonument::construire() { construit = 1; }
