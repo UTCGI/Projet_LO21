8e870b844c48b3b0be7be5cd1ebeb7023e6e66b0
josephine <josephine.60100@hotmail.fr>
2022-11-11 10:39:20 +0100

    monument.cpp/.h ajout attribut prix, methode get associee, affichage associe

--- Code/monument.cpp
+++ Code/monument.cpp
@@ -1,10 +1,10 @@
 #include "monument.h"
 
-Monument::Monument(const string& nom, const string& effet):nom(nom),effet(effet),nb_exemplaires(4){
+Monument::Monument(const string& nom, const string& effet, const unsigned int prix):nom(nom),effet(effet),prix(prix),nb_exemplaires(4){
  
 }
 
 ostream& operator<<(ostream& f,const Monument& m){
-    f<<"Monument : "<<m.getNom()<<endl<<"Effet : "<<m.getEffet()<<endl;
+    f<<"Monument : "<<m.getNom()<<endl<<"Effet : "<<m.getEffet()<<endl<<"Prix : "<<m.getPrix()<<endl<<"Quantite : "<<m.getExemplaire()<<endl;
     return f;
 };
--- Code/monument.h
+++ Code/monument.h
@@ -9,15 +9,17 @@ class Monument{
 private : 
     const string nom;
     const string effet;
+    const unsigned int prix;
     const unsigned int nb_exemplaires;
 public :
-    Monument(const string& nom, const string& effet);
+    Monument(const string& nom, const string& effet, const unsigned int prix);
     ~Monument() = default;
     Monument(const Monument& m) = delete;
     Monument& operator=(const Monument& m) = delete;
 
     string getNom()const {return nom;}
     string getEffet()const {return effet;}
+    unsigned int getPrix()const {return prix;}
     unsigned int getExemplaire()const {return nb_exemplaires;}
 };
 
