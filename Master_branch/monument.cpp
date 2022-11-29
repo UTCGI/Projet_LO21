98a81ef6c984935a03cd7adc8841795596fde9c8
gvandevi <115038668+gvandevi@users.noreply.github.com>
2022-11-29 23:18:26 +0100

    Create monument.cpp

---
+++ Master_branch/monument.cpp
@@ -0,0 +1,25 @@
+#include "monument.h"
+
+string Monument::getNom() const { return nom; }
+string Monument::getEffet() const { return effet; }
+unsigned int Monument::getPrix() const { return prix; }
+unsigned int Monument::getExemplaire() const { return nb_exemplaires; }
+
+Monument::Monument(const string& nom, const string& effet,
+                   const unsigned int prix) :
+    nom(nom), effet(effet), prix(prix), nb_exemplaires(4) {}
+
+ostream& operator<<(ostream& f, const Monument& m) {
+  f << "Nom \t\t\t: " << m.getNom() << endl
+    << "Effet \t\t\t: " << m.getEffet() << endl
+    << "Prix \t\t\t: " << m.getPrix() << " piece";
+  if (m.getPrix() > 1)    f << "s";
+  f << endl << "Quantite \t\t: " << m.getExemplaire() << endl << endl;
+  return f;
+};
+
+//TEST
+int MonumentTest(){
+    Monument* m1=new Monument("Gare","Vous pouvez lancer deux des",4);
+    cout<<*m1;
+}
