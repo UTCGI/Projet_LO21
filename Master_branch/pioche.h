742ff3f242636dc593a0f9b97b574dbb6e6f3d80
Gabrielle van de Vijver <gabrielle.van-de-vijver@etu.utc.fr>
2022-12-05 11:28:23 +0100

    testé et corrigé

--- Master_branch/pioche.h
+++ Master_branch/pioche.h
@@ -2,19 +2,21 @@
 #include "pile_etablissement.h"
 
 class Pioche {
- private:
-  *Pile_Etablissement[] piles_etablissement;
+    public:
+// private:
   unsigned int nb_etablissements;
   unsigned int nb_piles;
- public:
+  Pile_Etablissement* piles_etablissement;
+// public:
   Pioche(const Jeu& jeu);
   unsigned int getNb_etablissements() const;
   unsigned int getNb_piles() const;
+  Pile_Etablissement* getPiles_etablissement() const;
   void afficher()const;
   const Etablissement& getRandomEtablissement() const;
-  unsigned int getIndexPile(const Etablissement& etablissement);
-  void retirer_Etablissement(const Etablissement& etablissement, unsigned int quantité= 1);
+  unsigned int getIndexPile(const Etablissement& etablissement)const;
+  void retirer_Etablissement(const Etablissement& etablissement, unsigned int quantite= 1);
   ~Pioche();
-}
+};
 
 int testPioche();
