94b47bfa8154509d781a53e988c266a463134e79
Gabrielle van de Vijver <gabrielle.van-de-vijver@etu.utc.fr>
2022-12-13 21:12:21 +0100

    initialisation fichiers etablissements speciaux

---
+++ Dev/etablissements_speciaux.h
@@ -0,0 +1,12 @@
+#ifndef PARTIE_H
+#define PARTIE_H
+#include "jeu.h"
+#include "joueur.h"
+#include "reserve.h"
+#include "etablissements_speciaux.h"
+
+void traiterEtablissementSpecial(const string& nom);
+void traiterChaineDeTelevision();
+void traiterCentredAffaire();
+void traiterStade();
+#endif
