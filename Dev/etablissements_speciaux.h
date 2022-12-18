eeac129354936efea5d9feeacb1c27ab8b7f2c1a
Gabrielle van de Vijver <gabrielle.van-de-vijver@etu.utc.fr>
2022-12-18 13:22:02 +0100

    problems fixed

--- Dev/etablissements_speciaux.h
+++ Dev/etablissements_speciaux.h
@@ -3,10 +3,12 @@
 #include "jeu.h"
 #include "joueur.h"
 #include "reserve.h"
-#include "etablissements_speciaux.h"
+#include "etablissement.h"
+#include "joueur.h"
+#include "pile_etablissement.h"
 
-void traiterEtablissementSpecial(const string& nom);
+void traiterEtablissementSpecial(const string& nom,const Joueur& joueur);
 void traiterChaineDeTelevision();
 void traiterCentredAffaire();
-void traiterStade();
+void traiterStade(const vector<Joueur*>& joueurs,Joueur* j, Pile_Etablissement* p);
 #endif
