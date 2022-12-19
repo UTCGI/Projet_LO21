11713bf33f71ddbe0137a5759bae8fd1fa6e591d
Asperkk <yjf1847252006@gmail.com>
2022-12-19 23:37:52 +0100

    Supression QT pour le remplacer

--- QT_applicaion/effets_monuments.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "partie.h"
-//effet carte hotel de ville
-void effetHotelDeVille(Partie* p);
-void effetParcDAttractions(Partie* p);//Effet réaliser dans main.c lancer()
-void effetTourRadio(Partie* p);//Effet réaliser dans main.c lancer()
-void effetPort(Partie* p); //Effet réaliser dans main.c lancer()
-void effeAeroport(Partie* p);//Effet réaliser dans main.c menu() choix1
